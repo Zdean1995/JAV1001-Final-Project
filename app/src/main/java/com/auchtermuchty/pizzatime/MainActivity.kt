46b470c3b0c6dee60bf7c092a171530b5d96da31
Zachary <zdean1995@gmail.com>
2023-03-30 17:12:24 -0400

    Added recyclerView for toppings and added onclicklisteners for the items in it.  Added view binding.

--- app/src/main/java/com/auchtermuchty/pizzatime/MainActivity.kt
+++ app/src/main/java/com/auchtermuchty/pizzatime/MainActivity.kt
@@ -2,10 +2,19 @@ package com.auchtermuchty.pizzatime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.auchtermuchty.pizzatime.adapter.ToppingsAdapter
+import com.auchtermuchty.pizzatime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rcyToppings.adapter = ToppingsAdapter()
+        (binding.rcyToppings.adapter as ToppingsAdapter).toppingsStrings =
+            mutableListOf(*resources.getStringArray(R.array.toppings))
     }
 }
--- app/src/main/java/com/auchtermuchty/pizzatime/adapter/ToppingsAdapter.kt
+++ app/src/main/java/com/auchtermuchty/pizzatime/adapter/ToppingsAdapter.kt
@@ -1,15 +1,42 @@
 package com.auchtermuchty.pizzatime.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.widget.SwitchCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.auchtermuchty.pizzatime.R
 
-class ToppingsAdapter {
+class ToppingsAdapter:
+    RecyclerView.Adapter<ToppingsAdapter.ToppingsViewHolder>() {
 
-    private val toppingsStrings: List<String> = listOf(R.array.toppings.toString())
+    //https://www.techiedelight.com/convert-string-to-list-kotlin/
+    var toppingsStrings: MutableList<String> = mutableListOf()
+    var toppingsCount: Int = 0
 
     class ToppingsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val swtToppings = view.findViewById(R.id.swt_toppings) as SwitchCompat
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToppingsViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.toppings_item, parent, false)
+
+        return ToppingsViewHolder(adapterLayout)
+    }
+
+    override fun getItemCount(): Int = toppingsStrings.count()
+
+
+    override fun onBindViewHolder(holder: ToppingsViewHolder, position: Int) {
+        holder.swtToppings.text = toppingsStrings[position]
+        holder.swtToppings.setOnClickListener {
+            if(holder.swtToppings.isChecked){
+                toppingsCount++
+            }
+            else{
+                toppingsCount--
+            }
+        }
     }
 }
