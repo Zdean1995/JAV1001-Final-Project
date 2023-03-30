c2b3bbcae02e9f3a1a237c1c747fdbf3cffa5e5a
Zachary <zdean1995@gmail.com>
2023-03-30 18:05:04 -0400

    Added pizza sizes

--- app/src/main/java/com/auchtermuchty/pizzatime/MainActivity.kt
+++ app/src/main/java/com/auchtermuchty/pizzatime/MainActivity.kt
@@ -7,6 +7,7 @@ import com.auchtermuchty.pizzatime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var price: Int = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +17,44 @@ class MainActivity : AppCompatActivity() {
         binding.rcyToppings.adapter = ToppingsAdapter()
         (binding.rcyToppings.adapter as ToppingsAdapter).toppingsStrings =
             mutableListOf(*resources.getStringArray(R.array.toppings))
+
+        binding.txtPrice.text = getString(R.string.price_txt, 0)
+
+        binding.swtLarge.setOnClickListener{
+            binding.swtMedium.isClickable = true
+            binding.swtMedium.isChecked = false
+            binding.swtSmall.isClickable = true
+            binding.swtSmall.isChecked = false
+            binding.swtLarge.isClickable = false
+            price = 10
+            calculatePrice()
+        }
+
+        binding.swtMedium.setOnClickListener{
+            binding.swtLarge.isClickable = true
+            binding.swtLarge.isChecked = false
+            binding.swtSmall.isClickable = true
+            binding.swtSmall.isChecked = false
+            binding.swtMedium.isClickable = false
+            price = 8
+            calculatePrice()
+        }
+
+        binding.swtSmall.setOnClickListener{
+            binding.swtLarge.isClickable = true
+            binding.swtLarge.isChecked = false
+            binding.swtMedium.isClickable = true
+            binding.swtMedium.isChecked = false
+            binding.swtSmall.isClickable = false
+            price = 6
+            calculatePrice()
+        }
+
+        binding.swtLarge.callOnClick()
+
+    }
+
+    fun calculatePrice(){
+        binding.txtPrice.text = getString(R.string.price_txt, (binding.rcyToppings.adapter as ToppingsAdapter).toppingsCount + price)
     }
 }
